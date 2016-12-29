6c130c2dd529b081fb4ffeba3c410c1bb8b31936
lmm2012 <469611406@qq.com>
2016-12-29 17:25:23 +0800

    add demo

---
+++ 详情demo/resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1482997028504.js
@@ -0,0 +1,467 @@
+jQuery("#simulation")
+  .on("click", ".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Hotspot_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Panel_cuxiao"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Panel_guige"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Panel_fenxiang"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "condition": {
+                "datatype": "property",
+                "target": "#s-Image_8",
+                "property": "jimIsVisible"
+              },
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Image_8"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            },
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Image_8"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_9")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Panel_shuoming"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Hotspot_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("windowscroll", ".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .windowscroll", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Image_2",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimPlus",
+                        "parameter": [ {
+                          "action": "jimWindowScrollY"
+                        },"618" ]
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Image_3",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimWindowScrollY"
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Image_4",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimWindowScrollY"
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Dynamic_Panel_1",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimWindowScrollY"
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "0"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Hotspot_6",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimPlus",
+                        "parameter": [ {
+                          "action": "jimWindowScrollY"
+                        },"20" ]
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "281"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Image_8",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimPlus",
+                        "parameter": [ {
+                          "action": "jimWindowScrollY"
+                        },"64" ]
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "264"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                },
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Hotspot_7",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimPlus",
+                        "parameter": [ {
+                          "action": "jimWindowScrollY"
+                        },"20" ]
+                      }
+                    },
+                    "left": {
+                      "type": "movetoposition",
+                      "value": "334"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        },
+        {
+          "blocks": [
+            {
+              "condition": {
+                "action": "jimGreater",
+                "parameter": [ {
+                  "action": "jimWindowScrollY"
+                },"380" ]
+              },
+              "actions": [
+                {
+                  "action": "jimShow",
+                  "parameter": {
+                    "target": "#s-Image_4"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            },
+            {
+              "actions": [
+                {
+                  "action": "jimHide",
+                  "parameter": {
+                    "target": "#s-Image_4"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ 详情demo/resources/scroll-1482997028504.js
@@ -0,0 +1,14 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimMobile, {
+        "loadScrollBars": function() {
+            jQuery(".s-46d1a8e0-e896-4412-9260-5579a8c0587e .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
